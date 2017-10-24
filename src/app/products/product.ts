13764a6cbf95e4d98b9076fc2bb67276d57ac3c7
SAPIENT\kmalh4 <kmalhotra5@sapient.com>
2017-10-24 10:26:37 +0530

    Initial commit

---
+++ src/app/products/product.ts
@@ -0,0 +1,12 @@
+
+export interface IProduct {
+    productId:number;
+    productName:String;
+    productCode:String;
+    releaseDate:String;
+    price:number;
+    description:String;
+    starRating:number;
+    imageUrl:String;
+
+}
